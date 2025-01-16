c8e1eb0907f5447fc7ba3c540ee1ea778f68d471
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-01-16 08:19:55 +0200

    Edit: Dark Mode

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,21 @@
-let them = document.getElementById("them");
+let them = document.getElementById('them');
 them.onclick = () => {
-  if (document.body.classList.contains("dark-mode")) {
-    them.src = "assets/icons/dark.webp";
-    document.body.classList.remove("dark-mode");
+  if (document.body.classList.contains('dark-mode')) {
+    them.src = 'assets/icons/dark.webp';
+    document.body.classList.remove('dark-mode');
   } else {
-    document.body.classList.add("dark-mode");
-    them.src = "assets/icons/light.webp";
+    document.body.classList.add('dark-mode');
+    them.src = 'assets/icons/light.webp';
   }
 };
 
-let userInformation = document.getElementById("user_info");
+let userInformation = document.getElementById('user_info');
 function getData() {
-  let userData = "";
-    fetch(`https://api.github.com/users/AbdelrahmanELsokary`)
-      .then((res) => res.json())
-      .then((user) => {
-        userData = `
+  let userData = '';
+  fetch(`https://api.github.com/users/AbdelrahmanELsokary`)
+    .then((res) => res.json())
+    .then((user) => {
+      userData = `
         <div class="user">
         <img src="${user.avatar_url}">
         <h1>${user.login}</h1>
@@ -28,22 +28,22 @@ function getData() {
         </div>
           <ol id="repos_data" class="repos_data"></ol>
         `;
-        fetch(`https://api.github.com/users/AbdelrahmanELsokary/repos`)
-          .then((res) => res.json())
-          .then((repostories) => {
-            let displayRepos = "";
-            for (let i = 0; i < repostories.length; i++) {
-              displayRepos += `
+      fetch(`https://api.github.com/users/AbdelrahmanELsokary/repos`)
+        .then((res) => res.json())
+        .then((repostories) => {
+          let displayRepos = '';
+          for (let i = 0; i < repostories.length; i++) {
+            displayRepos += `
               <li>
               <span>${repostories[i].name}</span>
                 <br> 
                 <a href="${repostories[i].homepage}" target="_blank">View Project</a> 
                 <a href="${repostories[i].html_url}" target="_blank">View Code</a></li>
               `;
-              document.getElementById("repos_data").innerHTML = displayRepos;
-            }
-          });
-        userInformation.innerHTML = userData;
-      });
-  }
-  getData(userInformation)
+            document.getElementById('repos_data').innerHTML = displayRepos;
+          }
+        });
+      userInformation.innerHTML = userData;
+    });
+}
+getData(userInformation);
